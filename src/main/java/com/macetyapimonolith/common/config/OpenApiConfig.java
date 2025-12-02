f836fda11275fb88fcc830326aec7e866503d728
Myck <136953823+Kmykh@users.noreply.github.com>
2025-12-02 01:43:09 -0500

    feat: add watering automation logic in Plant Service

--- src/main/java/com/macetyapimonolith/common/config/OpenApiConfig.java
+++ src/main/java/com/macetyapimonolith/common/config/OpenApiConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 //implement MQTT connector for ESP32 communication
+// feat: add watering automation logic in Plant Service
 @Configuration
 public class OpenApiConfig {
 
