e660d7d0c5a30942f93e4df6be72c35657b750b0
Myck <136953823+Kmykh@users.noreply.github.com>
2025-12-02 01:42:41 -0500

    implement MQTT connector for ESP32 communication

--- src/main/java/com/macetyapimonolith/common/config/OpenApiConfig.java
+++ src/main/java/com/macetyapimonolith/common/config/OpenApiConfig.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//implement MQTT connector for ESP32 communication
 @Configuration
 public class OpenApiConfig {
 
